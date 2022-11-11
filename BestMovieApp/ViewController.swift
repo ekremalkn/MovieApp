c2154b3bcec1c501c1671a7137bc0e963c1a9b52
Ekrem Alkan <ae.alkanekrem@gmail.com>
2022-11-11 13:27:04 +0300

    Initial Commit

---
+++ BestMovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BestMovieApp
+//
+//  Created by Ekrem Alkan on 11.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
